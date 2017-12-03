505a69d7f9b66a1e7bc7e79210f2f650ebb58b02
Ben Harris <mail@benharris.ch>
2017-12-03 15:59:01 -0500

    switch to .net configurationbuilder instead of dotenv

--- Commands/Status.cs
+++ Commands/Status.cs
@@ -22,7 +22,6 @@ namespace dotbot.Commands
     [Group("sample")]
     public class Sample : ModuleBase<SocketCommandContext>
     {
-        // ~sample square 20 -> 400
         [Command("square")]
         [Summary("Squares a number.")]
         public async Task SquareAsync([Summary("The number to square.")] int num)
@@ -31,12 +30,6 @@ namespace dotbot.Commands
             await Context.Channel.SendMessageAsync($"{num}^2 = {Math.Pow(num, 2)}");
         }
 
-        // ~sample userinfo --> foxbot#0282
-        // ~sample userinfo @Khionu --> Khionu#8708
-        // ~sample userinfo Khionu#8708 --> Khionu#8708
-        // ~sample userinfo Khionu --> Khionu#8708
-        // ~sample userinfo 96642168176807936 --> Khionu#8708
-        // ~sample whois 96642168176807936 --> Khionu#8708
         [Command("userinfo")]
         [Summary("Returns info about the current user, or the user parameter, if one passed.")]
         [Alias("user", "whois")]
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Reflection;
@@ -12,38 +13,44 @@ namespace dotbot
     {
         private CommandService _commands;
         private DiscordSocketClient _client;
+        private IConfigurationRoot _config;
         private IServiceProvider _services;
 
         public static void Main(string[] args)
-            => new Program().MainAsync().GetAwaiter().GetResult();
+            => new Program().StartAsync().GetAwaiter().GetResult();
 
-        public async Task MainAsync()
+        public async Task StartAsync()
         {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("_config.json");
+            _config = builder.Build();
+            // get token from _config.json file
+            // if your bot isn't connecting, rename _config.example.json to _config.json
+            // and then place your bot token between the quotes
+            string token = _config["tokens:discord"];
+            Console.WriteLine(token);
+            
             _client = new DiscordSocketClient();
             _commands = new CommandService();
 
             _client.Log += Log;
 
-            DotNetEnv.Env.Load();
-            string token = Environment.GetEnvironmentVariable("TOKEN");
-
             _services = new ServiceCollection()
                 .AddSingleton(_client)
                 .AddSingleton(_commands)
+                .AddSingleton(_config)
+                .AddSingleton<Random>()
                 .BuildServiceProvider();
 
-            await InstallCommandsAsync();
+            // install all commands from the assembly
+            _client.MessageReceived += HandleCommandAsync;
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly());
 
             await _client.LoginAsync(TokenType.Bot, token);
             await _client.StartAsync();
 
             await Task.Delay(-1);
-        }
-
-        public async Task InstallCommandsAsync()
-        {
-            _client.MessageReceived += HandleCommandAsync;
-            await _commands.AddModulesAsync(Assembly.GetEntryAssembly());
         }
 
         private async Task HandleCommandAsync(SocketMessage arg)
@@ -53,7 +60,7 @@ namespace dotbot
 
             int argPos = 0;
 
-            if (!(message.HasCharPrefix(';', ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos))) return;
+            if (!(message.HasStringPrefix(_config["prefix"], ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos))) return;
 
             var context = new SocketCommandContext(_client, message);
 
