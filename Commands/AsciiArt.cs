0b5780cc2b487661791375d05ea4692856bc77e1
Ben Harris <ben@tilde.team>
2017-12-04 10:24:44 -0500

    add config to ascii class

--- Commands/AsciiArt.cs
+++ Commands/AsciiArt.cs
@@ -1,16 +1,24 @@
 ﻿using Discord.Commands;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace dotbot.Commands
 {
     public class AsciiArt : ModuleBase<SocketCommandContext>
     {
+        private readonly IConfigurationRoot _config;
+
+        public AsciiArt(IConfigurationRoot config) {
+            _config = config;
+        }
+
         [Command("ascii")]
         [Summary("creates ascii word art")]
         public async Task CreateAsciiArt(
-            [Summaery("font you want to use")] string fontName,
-        	[Remainder] [Summary("text to convert")] string ArtString
-		) {
+            [Summary("font you want to use")] string fontName,
+            [Remainder] [Summary("text to convert")] string ArtString
+        ) {
             if (fontName == "list") {
                 return await ReplyAsync($"available fonts for use with `{_config["prefix"]}ascii`:\n```{string.Join(", ", Directory.GetFiles("Fonts").Select(Path.GetFileNameWithoutExtension))}```");
             } else if (File.Exists($"Fonts/{fontName}.flf")) {
@@ -22,3 +30,4 @@ namespace dotbot.Commands
         }
     }
 }
+
